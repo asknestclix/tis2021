109331dca104169e1521a72635393bf86635eb50
Francis Robert Oca <asknestclix@gmail.com>
2021-11-11 14:05:25 -0500

    test json

--- assets/js/contactlist.js
+++ assets/js/contactlist.js
@@ -1,434 +1,49 @@
-let data = [
-    
-    {
-        "departmentName": "قسم القبول و التسجيل",
-        "otherInfo": "",
-        "contactList": [
-            {
-                "name": "الأستاذة تهاني الشهري",
-                "mobile": "0530634714",
-                "email": "واتس اب فقط"
-            },
-            {
-                "name": "الأستاذة نادية المقرن",
-                "mobile": "0530625524",
-                "email": "واتس اب فقط"
-            },
-            {
-                "name": "الأستاذة البندري عبدالمجيد",
-                "mobile": "",
-                "email": "alabdulmajeed@tischools.edu.sa"
-            }
-        ]
-    },
-    {
-        "departmentName": "قسم المالية",
-        "otherInfo": "",
-        "contactList": [
-            {
-                "name": "الأستاذة سارة الدوخي",
-                "mobile": "",
-                "email": "saldukhi@tischools.edu.sa"
-            },
-            {
-                "name": "الأستاذة بارعة قطنا",
-                "mobile": "",
-                "email": "bkattna@tischools.edu.sa"
-            }
-        ]
-    },
-    {
-        "departmentName": "قسم العلاقات العامة",
-        "otherInfo": "",
-        "contactList": [
-            {
-                "name": "البريد الالكتروني",
-                "mobile": "",
-                "email": "pr@tischools.edu.sa"
-            }
-        ]
-    },
-    {
-        "departmentName": "مرحلة رياض الأطفال",
-        "otherInfo": "الساعات المكتبية : من الساعة الرابعة حتى الساعة الخامسة مساء من الأحد إلى الخميس",
-        "contactList": [
-            {
-                "name": "مديرة المرحلة : خلود بنت عبد العزيز العفنان",
-                "mobile": "",
-                "email": "kalafnan@tischools.edu.sa"
-                
-            },
-            {
-                "name": "وكيلة الشؤون التعليمية : العنود آل اسماعيل",
-                "mobile": "",
-                "email": "aalismael@tischools.edu.sa"
-            },
-            {
-                "name": "وكيلة شؤون الطلاب : إبتسام الشريدة",
-                "mobile": "",
-                "email": "ealshareda@tischools.edu.sa"
-            },
-            {
-                "name": "المساعد الإداري : لينا بنت سامي الحربي",
-                "mobile": "",
-                "email": "lealharbi@tischools.edu.sa"
-            }
-        ]
-    },
-    {
-        "departmentName": "المرحلة الابتدائية الأولية",
-        "otherInfo" : "الساعات المكتبية : من الساعة الخامسة حتى الساعة السادسة مساء من الأحد إلى الخميس",
-        "contactList": [
-            {
-                "name": "مديرة المرحلة : سعاد بنت محمد العماري",
-                "mobile": "",
-                "email": "soalamari@tischools.edu.sa"
-            },
-            {
-                "name": "وكيلة الشؤون التعليمية : غادة بنت صبحي أمهان",
-                "mobile": "",
-                "email": "gamhan@tischools.edu.sa"
-            },
-            {
-                "name": "وكيلة شؤون الطالبات : روان السلوم",
-                "mobile": "",
-                "email": "raalsalloom@tischools.edu.sa"
-            },
-            {
-                "name": "الموجهة الطلابية(بنات) : الهنوف العياف",
-                "mobile": "",
-                "email": "haalayyaf@tischools.edu.sa"
-            },
-            {
-                "name": "الموجهة الطلابية(بنين) : نجلاء بن طالب",
-                "mobile": "",
-                "email": "nbintaleb@tischools.edu.sa"
-            },
-            {
-                "name": "الموجهة الطلابية(بنين) : نجلاء بن طالب",
-                "mobile": "",
-                "email": "nbintaleb@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الأول بنات: رندا الصدي",
-                "mobile": "",
-                "email": "ralsaddi@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الثاني بنات: العنود الرزقان",
-                "mobile": "",
-                "email": "aalrazgan@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الثالث بنات : نوف الأحمد",
-                "mobile": "",
-                "email": "nalahmad@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الأول و الثاني بنين : نورة القحطاني",
-                "mobile": "",
-                "email": "nalqahtani@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الثالث بنين : غالية السبيعي",
-                "mobile": "",
-                "email": "galsubaiey@tischools.edu.sa"
-            },
-            {
-                "name": "مسئولة الدعم التقني : فاطمة النور",
-                "mobile": "",
-                "email": "it@tischools.edu.sa"
-            },
-            {
-                "name": "هاتف و واتس اب المرحلة الابتدائية الأولية بنات : ",
-                "mobile": "0553304723",
-                "email": ""
-            },
-            {
-                "name": "هاتف و واتس اب المرحلة الابتدائية الأولية بنين : ",
-                "mobile": "0553301926",
-                "email": ""
-            }
-        ]
-    },
-    {
-        "departmentName": "المرحلة الابتدائية العليا",
-        "otherInfo": "الساعات المكتبية : من الساعة الثالثة حتى الساعة الخامسة مساء من الأحد إلى الخميس",
-        "contactList": [
-            {
-                "name": "مديرة المرحلة : حسناء بنت عبد الله العليان",
-                "mobile": "",
-                "email": "halolayan@tischools.edu.sa"
-            },
-            {
-                "name": "وكيلة شؤون الطالبات : لمياء بنت عماد معمر",
-                "mobile": "",
-                "email": "lmuammar@tischools.edu.sa"
-            },
-            {
-                "name": "الموجهة الطلابية : سحر المقبل",
-                "mobile": "",
-                "email": "saalmuqbil@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الرابع و الصف السادس ( شعبة أ و-ب): مشاعل بن عسكر",
-                "mobile": "",
-                "email": "maskar@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الخامس و السادس( شعبة ج - د) : لطيفة بن طالب",
-                "mobile": "",
-                "email": "ltaleb@tischools.edu.sa"
-            },
-            {
-                "name": "مسئولة الدعم التقني : فاطمة النور",
-                "mobile": "",
-                "email": "it@tischools.edu.sa"
-            },
-            {
-                "name": "هاتف و واتس المرحلة : ",
-                "mobile": "0553305703",
-                "email": ""
-            }
-        ]
-    },
-    {
-        "departmentName": "المرحلة المتوسطة",
-        "otherInfo": "الساعات المكتبية : من الساعة الثانية عشر حتى الساعة الواحدة ظهرا من الأحد إلى الخميس",
-        "contactList": [
-            {
-                "name": "مديرة المرحلة : غادة بنت أحمد الآغا",
-                "mobile": "",
-                "email": "galagha@tischools.edu.sa"
-            },
-            {
-                "name": "وكيلة شؤون الطالبات : بسمة أبو صالح",
-                "mobile": "",
-                "email": "baabusaleh@tischools.edu.sa"
-            },
-            {
-                "name": "الموجهة الطلابية : الهام النهدي",
-                "mobile": "",
-                "email": "elalnahdi@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الأول متوسط : عهد الهديب",
-                "mobile": "",
-                "email": "alhudaib@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الثاني متوسط : سحر الدوس",
-                "mobile": "",
-                "email": "saldous@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الثالث متوسط : هيا البوري",
-                "mobile": "",
-                "email": "halbori@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الثالث متوسط : عصماء العسكر",
-                "mobile": "",
-                "email": "aalaskar@tischools.edu.sa"
-            },
-            {
-                "name": "مسئولة الدعم التقني : وفاء الشطي",
-                "mobile": "",
-                "email": "it@tischools.edu.sa"
-            },
-            {
-                "name": "هاتف و واتس المرحلة",
-                "mobile": "0553302386",
-                "email": ""
-            }
-        ]
-    },
-    {
-        "departmentName": "المرحلة الثانوية",
-        "otherInfo": "الساعات المكتبية : من الساعة الثانية عشر حتى الساعة الواحدة ظهرا من الأحد إلى الخميس",
-        "contactList": [
-            {
-                "name": "مديرة المرحلة : غادة بنت أحمد الآغا",
-                "mobile": "",
-                "email": "galagha@tischools.edu.sa"
-            },
-            {
-                "name": "وكيلة شؤون الطالبات : ميساء بنت ثامر الدليمي",
-                "mobile": "",
-                "email": "maldulaimi@tischools.edu.sa"
-            },
-            {
-                "name": "الموجهة الطلابية : تهاني القريني",
-                "mobile": "",
-                "email": "taalgrany@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الأول ثانوي : إيمان الفريح",
-                "mobile": "",
-                "email": "ealfraih@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الثاني ثانوي : عصماء العسكر",
-                "mobile": "",
-                "email": "aalaskar@tischools.edu.sa"
-            },
-            {
-                "name": "إدارية الصف الثالث ثانوي : خلود السلطان",
-                "mobile": "",
-                "email": "kalsultan@tischools.edu.sa"
-            },
-            {
-                "name": "مسئولة الدعم التقني : وفاء الشنطي",
-                "mobile": "",
-                "email": "it@tischools.edu.sa"
-            },
-            {
-                "name": "هاتف و واتس اب المرحلة",
-                "mobile": "0553302951",
-                "email": ""
-            }
-        ]
-    },
-    {
-        "departmentName": "برنامج الدبلوما الأمريكية",
-        "otherInfo": "الساعات المكتبية : من الساعة الثانية عشر حتى الساعة الواحدة ظهراً من الأحد إلى الخميس",
-        "contactList": [
-            {
-                "name": "مديرة المرحلة : روى بنت نزار سمان",
-                "mobile": "",
-                "email": "rosamman@tischools.edu.sa"
-            },
-            {
-                "name": "وكيلة الشؤون التعليمية للصفوف 3-1 : عطارد الريس",
-                "mobile": "",
-                "email": "aalrayes@tischools.edu.sa"
-            },
-            {
-                "name": "وكيلة الشؤون التعليمية للصفوف 4-12 : رنيم دهمان",
-                "mobile": "",
-                "email": "rdahman@tischools.edu.sa"
-            },
-            {
-                "name": "وكيلة الدبلوما : حنان الثنيان",
-                "mobile": "",
-                "email": "haalthunayan@tischools.edu.sa"
-            },
-            {
-                "name": "الموجهة الطلابية : منال جاد الله",
-                "mobile": "",
-                "email": "magadallah@tischools.edu.sa"
-            },
-            {
-                "name": "المساعدة الإدارية : نوف الشهراني",
-                "mobile": "",
-                "email": "nalshahrani@tischools.edu.sa"
-            },
-            {
-                "name": "مسئولة الدعم التقني : مي أبو الليل",
-                "mobile": "",
-                "email": "it@tischools.edu.sa"
-            },
-            {
-                "name": "هاتف و واتس اب الصفوف من 1-6 : ",
-                "mobile": "0500351231",
-                "email": ""
-            },
-            {
-                "name": "هاتف و واتس اب الصفوف من 7-12",
-                "mobile": "0553301882",
-                "email": ""
-            }
-        ]
-    },
-    {
-        "departmentName": "قسم البنين",
-        "otherInfo": "الساعات المكتبية : من الساعة الواحدة حتى الساعة الثانية مساء من الأحد إلى الخميس",
-        "contactList": [
-            {
-                "name": "مدير المرحلة الابتدائية : حمدان بن ثاني العنزي",
-                "mobile": "",
-                "email": "halenazi@tischools.edu.sa"
-            },
-            {
-                "name": "مدير المرحلة المتوسطة و الثانوية : محمد آل بوراس",
-                "mobile": "",
-                "email": "mboras@tischools.edu.sa"
-            },
-            {
-                "name": "وكيل الشؤون التعليمية : علي سيد درويش",
-                "mobile": "",
-                "email": "asayed@tischools.edu.sa"
-            },
-            {
-                "name": "وكيل قسم الدبلوما : سلمان الدوسري",
-                "mobile": "",
-                "email": "saldosari@tischools.edu.sa"
-            },
-            {
-                "name": "وكيل شؤون الطلاب للمرحلة الابتدائية : إبراهيم الحربي",
-                "mobile": "",
-                "email": "ialharbi@tischools.edu.sa"
-            },
-            {
-                "name": "وكيل شؤون الطلاب للمرحلة المتوسطة : مفرح الراشد",
-                "mobile": "",
-                "email": "malrashid@tischools.edu.sa"
-            },
-            {
-                "name": "وكيل شؤون الطلاب للمرحلة الثانوية : خالد المطارنة",
-                "mobile": "",
-                "email": "kalmatarneh@tischools.edu.sa"
-            },
-            {
-                "name": "الموجة الطلابي للمرحلة الابتدائية : بدر العتيبي",
-                "mobile": "",
-                "email": "balotaibi@tischools.edu.sa"
-            },
-            {
-                "name": "الموجه الطلابي للمرحلة المتوسطة : عبدالله عسيري",
-                "mobile": "",
-                "email": "aasiri@tischools.edu.sa"
-            },
-            {
-                "name": "الموجه الطلابي للمرحلة الثانوية : لؤي بشتاوي",
-                "mobile": "",
-                "email": "lbashtawi@tischools.edu.sa"
-            },
-            {
-                "name": "مسئول الدعم التقني : وائل عبدالقادر",
-                "mobile": "",
-                "email": "it@tischools.edu.sa"
-            },
-            {
-                "name": "هاتف و واتس اب المرحلة",
-                "mobile": "0553303295",
-                "email": ""
-            }
-        ]
-    },
-    {
-        "departmentName": "الإدارة العامة",
-        "otherInfo": "",
-        "contactList": [
-            {
-                "name": "المديرة العامة : جوهرة بنت أحمد التويجري",
-                "mobile": "",
-                "email": "jaltuwaijri@tischools.edu.sa"
-            },
-            {
-                "name": "مديرة إدارة الشؤون المدرسية : منيرة السعدون",
-                "mobile": "",
-                "email": "malsadoon@tischools.edu.sa"
-            },
-            {
-                "name": "مديرة برنامج حياة للإرشاد الطلابي: هناء السليماني",
-                "mobile": "",
-                "email": "halsulaimani@tischools.edu.sa"
-            }
-        ]
-    }
-];
+let departmentList = null;
+let contactList = null;
+let data = null;
 
+const loadAPI = () => {
+    $.getJSON( "http://167.172.52.246:1337/contact-lists", function( data ) {
+        departmentList = data;
+    }).done(()=>{
+        $.getJSON( "http://167.172.52.246:1337/dpartment-contact-lists", function( data ) {
+            contactList = data;
+        }).done(()=>{
+            contructData(departmentList, contactList);
+            populateSelect();
+        });
+    });
+    
+}
+
+const contructData = (department, contactList) => {
+   let retVal = [];
+
+   if (department && department.length>0) {
+       department.forEach(dep => {
+
+            if (contactList && contactList.length) {
+                let contactItems = [];
+                contactList.forEach(contact => {
+                    if (contact.department.Tag === dep.Tag) {
+                        contactItems.push({
+                            'name': contact.name,
+                            'mobile': contact.mobile || "",
+                            'email' : contact.emailAddress || ""
+                        }); 
+                    }
+                });
+
+                let newContact = {'departmentName' : dep.departmentName,
+                                  'otherInfo' : dep.otherInfo || "",
+                                  'contactList' : contactItems};
+                retVal.push(newContact);                  
+            }
+       });
+   }
+   data = retVal;
+
+}
 
 
 function populateSelect() {
@@ -468,6 +83,7 @@ activities.addEventListener("change", function(e) {
     let innerHtm = "";
     if(e.target.value) {
         data.forEach( (el,index) => {
+            debugger;
             if (el.departmentName === e.target.value) {
                 let element = document.getElementById("contents");
                 element.style.display="block";
@@ -493,4 +109,4 @@ activities.addEventListener("change", function(e) {
 
 
 
-window.onload = populateSelect();
+window.onload = loadAPI();
