012bb67ea9107efd4232a850d43a9da3734f318e
Francis Robert Oca <asknestclix@gmail.com>
2021-11-29 15:30:59 -0500

    new updates

--- assets/js/modalMain.js
+++ assets/js/modalMain.js
@@ -31,12 +31,25 @@ const showModal3 = () => {
     $('#home').addClass('blur');
     $('#cont').addClass('blur');
     $('.cont2').addClass('blur');
-    //$('body').addClass('noScroll');
+    $('body').addClass('noScroll');
     
   }
 
 
 }
+const showModal4 = () => {
+  
+  $('.modal4').addClass('open');
+
+  if ($('.modal4').hasClass('open')) {
+    $('#home').addClass('blur');
+    $('#cont').addClass('blur');
+    $('.cont2').addClass('blur');
+    $('body').addClass('noScroll');
+    
+  }
+}
+
 $('#q1').click(function () {
     
   showModalMain();
@@ -57,10 +70,15 @@ $('#addy2').click(function () {
   showModal3();
   
 });
+$('#onlineReg').click(function () {
+  showModal4();
+  
+});
 $('.close').click(function () {
     $('.modal').removeClass('open');
     $('.modal2').removeClass('open');
     $('.modal3').removeClass('open');
+    $('.modal4').removeClass('open');
     $('#home').removeClass('blur');
     $('#cont').removeClass('blur');
     $('.cont2').removeClass('blur');
